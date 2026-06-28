0aa4f058128af2357dee24bc5628ab408ccfe577
Dmitry Sapelnikov <dmitry.sapelnikov@gmail.com>
2026-06-28 02:09:21 +0200

    Added: extra comments for get_fibonacci() definition

--- include/fibonacci.h
+++ include/fibonacci.h
@@ -1,6 +1,9 @@
 #pragma once
 
 /**
-    Returns the i-th Fibonacci number (0-indexed).
+    Returns the i-th Fibonacci number (0-indexed)
+	\param index The index of the Fibonacci number to return
+	\return The i-th Fibonacci number
+	\note Returns an overflowed result for index >= 94
  */
-[[nodiscard]] unsigned long long get_fibonacci(const unsigned index) noexcept;
+[[nodiscard]] unsigned long long get_fibonacci(unsigned index) noexcept;
